b28171dff22989d59bffcd7d2ebd410f26b5bb17
Anselmo <anselmodiasyahoo@gmail.com>
2022-01-06 11:25:51 -0300

    comit

--- src/components/RepositoryList.tsx
+++ src/components/RepositoryList.tsx
@@ -21,22 +21,14 @@ export function RepositoryList() {
     }, []);
 
 
-
-
-
     return(
         <section className="repository-list">
         <h1>Lista de Repositorios</h1>
 
             <ul>
                 {repositories.map(repository => {
-
                     return <RepositoryItem  key={repository.name} repository = {repository} />
-
-                      }
-                    )
-                  }
-               
+                })}
             </ul>
         
         </section>
